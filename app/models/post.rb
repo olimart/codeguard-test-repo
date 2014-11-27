b5cd5cfa833dc75fec11ea281ff4ba078d8a9b50
Olivier <olivier.simart@gmail.com>
2014-11-27 11:13:16 -0500

    testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,17 @@ class Post < ActiveRecord::Base
 	# ------------------------------------------------------------------------------------------------------
 	belongs_to :user, counter_cache: true
 
+
+	# VALIDATIONS
+	# ------------------------------------------------------------------------------------------------------
+	validates :title, presence: true
+
+
+  # INSTANCE METHODS
+  # ------------------------------------------------------------------------------------------------------
+	def camelMethod
+		#non-formatted comment
+		puts "Should raise issue"
+	end
+
 end
